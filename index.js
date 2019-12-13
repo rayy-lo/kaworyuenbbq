1d21617f86f5209f4a9f31b01a579d27fcd6530e
Raymond Lo <raymond.lo@yahoo.com>
2019-12-13 18:50:13 -0500

    removed sending email fn

--- index.js
+++ index.js
@@ -1,37 +1,3 @@
-document.getElementById('serverless-contact-form').addEventListener('submit', sendDataToLambda);
-
-function sendDataToLambda(e) {
-    e.preventDefault();
-    var formEmail = document.querySelector('#email').value;
-    var formSubject = document.querySelector('#subject').value;
-    var formMessage = document.querySelector('#message').value;
-    var endpoint = 'https://5xoj21h9ok.execute-api.us-east-1.amazonaws.com/prod/send-email-lambda';
-    var body = {
-        email: formEmail,
-        subject: formSubject,
-        message: formMessage
-    }
-
-    fetch(endpoint, {
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(body)
-    })
-      .then(response => {
-        if(response.ok){
-          document.getElementById('sentmsg').textContent = 'Email has been sent. Thank you!'
-          document.getElementById('serverless-contact-form').reset();
-        } else {
-          document.getElementById('sentmsg').textContent = 'Error'
-          throw Error (`Request rejected with status ${res.status}`)
-        }
-      })
-      .catch(err => console.error(err));
-}
-
 var animateHTML = function() {
     var elems;
     var windowHeight;
